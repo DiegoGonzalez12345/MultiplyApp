5422fd57af5724f99ebbfc5782ef7b90be104a03
Diego Gonzalez <dzbreez@outlook.com>
2021-09-27 10:32:31 -0600

    MVP

--- MultiplyApp/ViewController.swift
+++ MultiplyApp/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    //Variable Declaration
+    @IBOutlet weak var textBox1: UITextField!
+    @IBOutlet weak var textBox2: UITextField!
+   
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+  
+    
+    
+    
+        
+        
+        
+    
+    
 
-
+    @IBAction func myButton(_ sender: Any)
+    {
+        var Number1=textBox1.text ?? "0"
+        var Number2=textBox2.text ?? "0"
+        var integerNumber1 = Int(Number1) ?? 0
+        
+        
+    }
+    
 }
 
