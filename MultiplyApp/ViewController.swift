61cc777b03b9aabe3bbf42f48e74ebe273d5aee0
Diego Gonzalez <dzbreez@outlook.com>
2021-09-28 10:18:56 -0600

    Stretch #1

--- MultiplyApp/ViewController.swift
+++ MultiplyApp/ViewController.swift
@@ -6,6 +6,47 @@
 //
 
 import UIKit
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 class ViewController: UIViewController
 {
@@ -19,10 +60,24 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        assignbackground()
+        textBox1.resignFirstResponder()
+        textBox2.resignFirstResponder()
     }
   
-    
+    func assignbackground()
+    {
+            let background = UIImage(named: "numbersbackground")
+
+            var imageView : UIImageView!
+            imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.image = background
+            imageView.center = view.center
+            view.addSubview(imageView)
+            self.view.sendSubviewToBack(imageView)
+    }
     
     
         
@@ -35,12 +90,17 @@ class ViewController: UIViewController
     {   //calculate
         var Number1=textBox1.text ?? "0"
         var Number2=textBox2.text ?? "0"
-        var integerNumber1 = Int(Number1) ?? 0
-        var integerNumber2 = Int(Number2) ?? 0
+        var integerNumber1 = Float(Number1) ?? 0
+        var integerNumber2 = Float(Number2) ?? 0
         var product=integerNumber1*integerNumber2
         var product1 = String(product)
         myLabel.text = product1
+        
+       
+        
     }
+    
+    
     
 }
 
